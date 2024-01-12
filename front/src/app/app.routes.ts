66ff0099223a51304f892d9cf2b5671e2ac8c3ba
Vladislav Shkvarun <neshkvarun000@gmail.com>
2024-01-12 14:04:27 +0100

    feat: add about and 404

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { P404Component } from './pages/p404/p404.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'p404', component: P404Component},
+    {path: '**', redirectTo: "/p404"},
+];
+
---
+++ front/src/app/pages/p404/p404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-p404',
+  standalone: true,
+  imports: [],
+  templateUrl: './p404.component.html',
+  styleUrl: './p404.component.scss'
+})
+export class P404Component {
+
+}
