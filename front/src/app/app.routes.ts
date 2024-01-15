66657b6fc57e0a227a15c930d5a4c1854af03f95
Vladislav Shkvarun <neshkvarun000@gmail.com>
2024-01-15 13:42:47 +0100

    feat: add layout structure

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { P404Component } from './pages/p404/p404.component';
 import { AboutComponent } from './pages/about/about.component';
+import { GameComponent } from './pages/game/game.component';
 
 export const routes: Routes = [
     {path: '', component: MainComponent},
     {path: 'about', component: AboutComponent},
     {path: 'p404', component: P404Component},
+    {path: 'game', component: GameComponent},
     {path: '**', redirectTo: "/p404"},
 ];
 
--- front/src/app/pages/main/main.component.ts
+++ front/src/app/pages/main/main.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+  play() {
+    
+  }
 }
