b2a846593861cb692e091f91ae3d3808d788a3d2
Vladislav Shkvarun <neshkvarun000@gmail.com>
2024-02-02 13:31:08 +0100

    feat: add form module

--- front/src/app/pages/game/game.component.ts
+++ front/src/app/pages/game/game.component.ts
@@ -2,22 +2,41 @@ import { Component } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { SocketService } from '../../services/socket.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 interface User {
   username: string;
   id: string;
 }
 
+interface Error {
+  isEmpty: boolean,
+  tooLong: boolean
+}
+
+interface EnterForm {
+  username: string,
+  error: Error
+}
+
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 
 export class GameComponent {
   public connectedUsers: User[] = [];
+  public enterForm: EnterForm = {
+    username: '',
+    error: {
+      isEmpty: false,
+      tooLong: false
+    }
+  };
+
   constructor(
     private socket: Socket,
     private socketService: SocketService,
@@ -34,11 +53,16 @@ export class GameComponent {
     this.socket.emit("get-my-info", (userInfo: User) => {
       console.log(userInfo);
       if (userInfo.username === "anonimus") {
-        // show dialog 
-        // get dioalog data from dialog
-        // validate data "not emty"
-        // send username to backend
+        
       }
     });
+  }
+
+  submitName() {
+    let input = document.getElementById("inputName") as HTMLInputElement;
+    let err = document.querySelector(".error") as HTMLElement;
+    if (input.value === "") {
+      err.style.opacity = "1";
+    }
   }
 }
